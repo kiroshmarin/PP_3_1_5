77fd81226da9834d9fee66731a1c9dcf58de4c45
Kirill_Oshmarin <k.oshmarin@gmail.com>
2023-07-05 19:43:10 +0300

    PP_3_1_4 Done!

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,18 +1,17 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.servicies.RoleService;
-import ru.kata.spring.boot_security.demo.servicies.AppUserDetailsService;
 import ru.kata.spring.boot_security.demo.servicies.UserService;
 import ru.kata.spring.boot_security.demo.utils.UserValidator;
 
 import javax.validation.Valid;
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
@@ -21,18 +20,17 @@ public class AdminController {
     private final UserService userService;
     private final RoleService roleService;
     private final UserValidator userValidator;
-    private final AppUserDetailsService appUserDetailsService;
 
-    public AdminController(UserService userService, RoleService roleService, UserValidator userValidator, AppUserDetailsService appUserDetailsService) {
+    public AdminController(UserService userService, RoleService roleService, UserValidator userValidator) {
         this.userService = userService;
         this.roleService = roleService;
         this.userValidator = userValidator;
-        this.appUserDetailsService = appUserDetailsService;
     }
 
     @GetMapping
-    public String index(Principal principal, Model model) {
-        model.addAttribute("authUser", appUserDetailsService.findByEmail(principal.getName()));
+    public String index(Authentication authentication, Model model) {
+        User user = (User) authentication.getPrincipal();
+        model.addAttribute("authUser", user);
         model.addAttribute("users", userService.index());
         model.addAttribute("rolesToAdd", roleService.getRoles());
         return "admin-panel";
@@ -55,19 +53,9 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-//    @GetMapping("/{id}/edit")
-//    public String edit(@PathVariable("id") Long id, Model model) {
-//        model.addAttribute("rolesToAdd", roleService.getRoles());
-//        model.addAttribute("user", userService.show(id));
-//        return "edit";
-//    }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("rolesToAdd", roleService.getRoles());
-            return "admin-panel";
-        }
+    public String update(@ModelAttribute("user")User user) {
         userService.update(user);
         return "redirect:/admin";
     }
